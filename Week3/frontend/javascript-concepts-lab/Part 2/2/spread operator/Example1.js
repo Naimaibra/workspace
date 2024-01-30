d8db08ceaaa83750d57308414f186322f5a981d6
Naima <Naima.IbrahimAbdirahman@metropolia.fi>
2024-01-30 15:55:43 +0200

    Remove week3 folder and its contents

--- Week3/frontend/javascript-concepts-lab/Part 2/2/spread operator/Example1.js
+++
@@ -1,5 +0,0 @@
-const originalArray = [10, 20, 30];
-const newArray = [...originalArray, 40, 50];
-
-console.log('Original Array:', originalArray);
-console.log('New Array with Additional Elements:', newArray);
